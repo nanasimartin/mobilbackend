2f5c69e9a62ba78123b0e8945399d4eb361fd8f0
nanasimartin <nanasimartin5@gmail.com>
2022-01-17 12:17:56 +0100

    almost

--- backend.js
+++ backend.js
@@ -35,7 +35,30 @@ app.get('/kerdesek', (req, res) => {
 
   })
 
- 
+  app.post('/beerkezett', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'foci'
+    })
+    
+    connection.connect()
+    
+    let dt=new Date();
+    let teljesdat=dt.getFullYear()+"-"+  (dt.getMonth()+1)   +"-"+dt.getDate();
+    connection.query("INSERT INTO beerkezett VALUES (NULL, '"+req.body.beviteltomb[1]+"', '"+req.body.beviteltomb[2]+"','"+req.body.beviteltomb[3]+"','"+req.body.beviteltomb[4]+"','"+req.body.beviteltomb[5]+"','"+req.body.beviteltomb[6]+"','"+req.body.beviteltomb[7]+"','"+req.body.beviteltomb[8]+"','"+req.body.bevitel1+"', '"+teljesdat+"') ", function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log("Sikeres kitoltes!")
+
+      res.send("Sikeres kitoltes!")
+    })
+    
+    connection.end()    
+
+  })  
 
 
 
