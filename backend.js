5a2bbe92c4b96de45422cd2f0e7475f2638fb8ca
nanasimartin <nanasimartin5@gmail.com>
2022-03-09 12:40:39 +0100

    0309

--- backend.js
+++ backend.js
@@ -35,6 +35,54 @@ app.get('/kerdesek', (req, res) => {
 
   })
 
+  app.post('/kommentfelv', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'foci'
+    })
+    
+    connection.connect()
+    
+    let dt=new Date();
+    let teljesdat=dt.getFullYear()+"-"+  (dt.getMonth()+1)   +"-"+dt.getDate();
+    connection.query("INSERT INTO uzenet VALUES (NULL, '"+req.body.bevitel1+"', '"+req.body.bevitel2+"', '"+teljesdat+"') ", function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log("Sikeres felvitel!")
+
+      res.send("Sikeres felvitel!")
+    })
+    
+    connection.end()    
+
+  })
+
+  app.get('/tema', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'foci'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from uzenet ORDER BY uzenet_id DESC ', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+  
+      res.send(rows)
+    })
+    
+    connection.end()    
+  
+  })
+
   app.post('/beerkezett', (req, res) => {
     var mysql = require('mysql')
     var connection = mysql.createConnection({
